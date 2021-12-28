e3693bf26dc13a8c7877450485a6b97430c5363a
hasy8007 <hasy8007@gmail.com>
2021-12-28 12:45:25 +0330

    first commit

---
+++ lib/login_ui/sing_up_page.dart
@@ -0,0 +1,119 @@
+import 'package:flutter/material.dart';
+
+class SingUpPage extends StatefulWidget {
+  const SingUpPage({Key? key}) : super(key: key);
+
+  @override
+  _SingUpPageState createState() => _SingUpPageState();
+}
+
+class _SingUpPageState extends State<SingUpPage> {
+  bool isActive = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(
+                height: MediaQuery.of(context).size.height * .06,
+              ),
+              Image.asset(
+                'assets/images/sign_up.png',
+                width: 260,
+                height: 220,
+                fit: BoxFit.fill,
+              ),
+              const SizedBox(
+                height: 34,
+              ),
+              const Text(
+                'Sing up',
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 28,
+                    color: Colors.black),
+              ),
+              const SizedBox(
+                height: 40,
+              ),
+              const TextField(
+                decoration: InputDecoration(
+                    hintText: 'Full Name',
+                    hintStyle: TextStyle(color: Colors.grey, fontSize: 14),
+                    prefixIcon: Icon(
+                      Icons.person_outline_sharp,
+                      size: 22,
+                      color: Color(0xfff97d7d),
+                    )),
+              ),
+              const SizedBox(
+                height: 14,
+              ),
+              const TextField(
+                decoration: InputDecoration(
+                    hintText: 'Email',
+                    hintStyle: TextStyle(color: Colors.grey, fontSize: 14),
+                    prefixIcon: Icon(
+                      Icons.email_outlined,
+                      color: Color(0xfff97d7d),
+                    )),
+              ),
+              const SizedBox(
+                height: 14,
+              ),
+              const TextField(
+                decoration: InputDecoration(
+                    hintText: 'Password',
+                    hintStyle: TextStyle(color: Colors.grey, fontSize: 14),
+                    prefixIcon: Icon(
+                      Icons.edit,
+                      color: Color(0xfff97d7d),
+                    )),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Checkbox(
+                    value: isActive,
+                    onChanged: (value) => setState(() {
+                      isActive = value!;
+                    }),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 18.0),
+                    child: Text(
+                      'I agree whit the terms and conditions\nand privacy policy',
+                      style: TextStyle(color: Colors.grey[400] , fontSize: 10),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              SizedBox(
+                width: MediaQuery.of(context).size.width,
+                height: 60,
+                child: MaterialButton(
+                  color: const Color(0xfff97d7d),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8)),
+                  child: const Text(
+                    'Sing up',
+                    style: TextStyle(color: Colors.white, fontSize: 16),
+                  ),
+                  onPressed: () {},
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
